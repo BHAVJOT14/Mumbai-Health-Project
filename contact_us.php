0dc79001b7d76ea8dc761f344b1c216e2ef37f6a
bhavi140201 <68502679+bhavi140201@users.noreply.github.com>
2021-10-28 18:40:38 +0530

    Updated the Navigation Links

--- contact_us.php
+++ contact_us.php
@@ -1,37 +1,37 @@
 <?php
 
 $message_sent = false;
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 if (isset($_POST['mail']) && $_POST['mail'] != '') {
 
-  if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
+    if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
 
-    $userName = $_POST['name'];
-    $userEmail = $_POST['mail'];
-    $messageSubject = $_POST['subject'];
-    $message = $_POST['message'];
-    
-    $to = "mumbaihealthproject@gmail.com";         // Change the "To" mail id 
-    $body = "<html>
+        $userName = $_POST['name'];
+        $userEmail = $_POST['mail'];
+        $messageSubject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $to = "mumbaihealthproject@gmail.com";         // Change the "To" mail id 
+        $body = "<html>
     <head>
     <title>Message From Contact Us page</title>
     </head>
     <body>
-    <h2>From: ". $userName ."</h2>
-    <p>Email: ". $userEmail ."</p>
-    <p>". $message ."</p>
+    <h2>From: " . $userName . "</h2>
+    <p>Email: " . $userEmail . "</p>
+    <p>" . $message . "</p>
     </body>
     </html>";
-    $from ="support@mumbaihealthproject.com";
-    
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= "From:" .$from;
-    mail($to, $messageSubject, $body, $headers);
+        $from = "support@mumbaihealthproject.com";
 
-    $message_sent = true;
-  }
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "From:" . $from;
+        mail($to, $messageSubject, $body, $headers);
+
+        $message_sent = true;
+    }
 }
 ?>
 
@@ -43,13 +43,10 @@ if (isset($_POST['mail']) && $_POST['mail'] != '') {
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- <meta /> -->
-    <meta charset="UTF-8" name="description"
-        content=" -A non-profit initiative dedicated towards equalising healthcare services in a city riddled with inequalities" />
+    <meta charset="UTF-8" name="description" content=" -A non-profit initiative dedicated towards equalising healthcare services in a city riddled with inequalities" />
     <title>Mumbai health project</title>
     <link rel="stylesheet" href="main.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-        crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 </head>
 
 <body class="square-grid full-size">
@@ -57,7 +54,7 @@ if (isset($_POST['mail']) && $_POST['mail'] != '') {
     <header class="site-header bg-white">
         <div class="wrapper site-header__wrapper">
             <img class="brand" src="images\logo_V1.jpg" style="width: 60px; margin: auto; margin-bottom: 2px" />
-            <a href="/index.html" style="margin-bottom: 0rem" class="brand">MUMBAI HEALTH PROJECT FOUNDATION</a>
+            <a href="./index.html" style="margin-bottom: 0rem" class="brand">MUMBAI HEALTH PROJECT FOUNDATION</a>
             <nav class="nav">
                 <button class="nav__toggle" aria-expanded="false" type="button">
                     <i class="fas fa-bars"></i>
@@ -84,35 +81,35 @@ if (isset($_POST['mail']) && $_POST['mail'] != '') {
 
 
         <?php
-    if ($message_sent) {
+        if ($message_sent) {
 
 
-    ?>
+        ?>
 
-        <h2 style="text-align: center;">Thanks, We will get in touch very soon</h2>
+            <h2 style="text-align: center;">Thanks, We will get in touch very soon</h2>
 
-        <!-- I have not done the modal now after two days that will be also completed -->
-
-        <?php
-    } else {
-    ?>
-
-        <div class="leftside">
-            <form class="contact-form" action="contact_us.php" method="post">
-                <h1 class="c_us">Contact Us</h1>
-                <input type="text" id="txt_input" name="name" placeholder="Full name" />
-                <br />
-                <input type="text" id="txt_input" name="mail" placeholder="your e-mail" /><br />
-                <input type="text" id="txt_input" name="subject" placeholder="subject" /><br />
-                <textarea name="message" placeholder="Message" id="message_area"></textarea><br />
-
-                <button type="submit" class="btn" name="submit">Send E-mail</button>
-            </form>
-        </div>
+            <!-- I have not done the modal now after two days that will be also completed -->
 
         <?php
-    };
-    ?>
+        } else {
+        ?>
+
+            <div class="leftside">
+                <form class="contact-form" action="contact_us.php" method="post">
+                    <h1 class="c_us">Contact Us</h1>
+                    <input type="text" id="txt_input" name="name" placeholder="Full name" />
+                    <br />
+                    <input type="text" id="txt_input" name="mail" placeholder="your e-mail" /><br />
+                    <input type="text" id="txt_input" name="subject" placeholder="subject" /><br />
+                    <textarea name="message" placeholder="Message" id="message_area"></textarea><br />
+
+                    <button type="submit" class="btn" name="submit">Send E-mail</button>
+                </form>
+            </div>
+
+        <?php
+        };
+        ?>
 
 
         <div class="rightside">
@@ -133,11 +130,9 @@ if (isset($_POST['mail']) && $_POST['mail'] != '') {
                     <div class="socials">
                         <h4>Follow Us</h4>
                         <a href="https://www.facebook.com/hargullyhealthy"><i class="fab fa-facebook"></i></a>
-                        <a href="https://instagram.com/mumbaihealthproject?utm_medium=copy_link"><i
-                                class="fab fa-instagram"></i></a>
+                        <a href="https://instagram.com/mumbaihealthproject?utm_medium=copy_link"><i class="fab fa-instagram"></i></a>
                         <a href="https://twitter.com/hargullyhealthy?s=11"><i class="fab fa-twitter"></i></a>
-                        <a href="https://www.linkedin.com/company/mumbai-health-project/"><i
-                                class="fab fa-linkedin"></i></a>
+                        <a href="https://www.linkedin.com/company/mumbai-health-project/"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
                 <div class="footer-col">
@@ -162,7 +157,7 @@ if (isset($_POST['mail']) && $_POST['mail'] != '') {
     </footer>
     <!-- Footer ends here -->
 
-    <script type="text/javascript" src="main.js"></script>
+    <script type="text/javascript" src="./main.js"></script>
 </body>
 
 </html>
